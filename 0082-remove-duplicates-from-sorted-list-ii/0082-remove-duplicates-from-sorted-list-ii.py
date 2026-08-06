2ea2644d80589057a93d8ad100b96dbc50631988
Praveen Naik <praveennaik9538@gmail.com>
2026-08-06 12:43:56 +0530

    Time: 2 ms (31.7%), Space: 19.4 MB (6.32%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy=ListNode(0,head) #initialization of dummy node
+        dummy=ListNode(0,head) #initialization of dummy node and make it point to node pointed by head remeber head is also a pointer
         prev=dummy #prev pointing to dummy node to track unique nodesand not duplicates 
         while head:
             if head.next and head.val == head.next.val: #compare head next values with current node
@@ -15,5 +15,5 @@ class Solution:
             else:
                 prev=prev.next# link prev next if not duplicate node 
             head = head.next #update head
-        return dummy.next #return dummy next which is pointing head 
+        return dummy.next #return dummy next which is pointing the start node and then the start node is further linked to other nodes using prev and head pointers rememeber head and prev are not nodes they are just pointers(references) that are connecting the nodes
         
