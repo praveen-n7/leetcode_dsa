e592572d9c29042d57f36ad6dfc80e36be6bea17
Praveen Naik <praveennaik9538@gmail.com>
2026-08-23 01:08:47 +0530

    Time: 4 ms (10.88%), Space: 19.3 MB (62.43%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         seen=set()
-        while n !=1 and n not in seen: #check for n not 1 and n not seen that is new elements not the repatedd ones
+        while n !=1 and n not in seen: # this condition will lead to returning of false,check for n not 1 and n not seen that is new elements not the repatedd ones 
             seen.add(n)
             n=sum(int(d)**2 for d in str(n))#sum the squares convert the int input to string type for easy computaion
         return n==1 #returns true
