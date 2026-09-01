c5db61629516f03f91ff52986cab6b48801c3329
Praveen Naik <praveennaik9538@gmail.com>
2026-09-01 14:48:24 +0530

    Time: 0 ms (100%), Space: 19.3 MB (68.88%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -2,7 +2,7 @@ class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         out=''
         while columnNumber>0:
-            columnNumber-=1 #first char starts from zero that is A 
+            columnNumber-=1 # modulo operator % works with 0 A 1 B 2 C 
             out = chr(65+columnNumber%26)+out #add the corresponding char to empty string out only the capital char added thats why numbers after 65 
             columnNumber//=26 #update columnnumber to terminate the loop and also process whole number 
         return out
