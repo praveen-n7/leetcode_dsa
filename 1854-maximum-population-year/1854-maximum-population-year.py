e508830f2ae30f18b795fc1e791d4590207f7c12
Praveen Naik <praveennaik9538@gmail.com>
2026-09-15 22:32:26 +0530

    Time: 55 ms (11.28%), Space: 19.3 MB (68.32%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.py
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -1,10 +1,10 @@
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
         best,best_year=0,1950
-        for y in range(1950,2051):
-            count = sum(1 for b,d in logs if b<=y<d)
+        for y in range(1950,2051): #range from 1950 stsrt point 2051 end point 
+            count = sum(1 for b,d in logs if b<=y<d) #checking of no of person alive condition logic 
             if count > best:
                 best = count
                 best_year=y
-        return best_year
+        return best_year #returning best year y from range mentioned above 
         
